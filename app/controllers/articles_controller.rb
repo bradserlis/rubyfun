5e44781a23ac2412ae3eed59cebeeac823d50861
bradserlis <bradserlis@yahoo.com>
2019-03-05 17:37:42 -0800

    error catching for create catch if title is less than 5 characters or no title present

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
     def new
+        @article = Article.new
+    end
+
+    def index
+        @articles = Article.all
     end
 
     def show
@@ -9,8 +14,11 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
 
-        @article.save
+      if @article.save
         redirect_to @article
+      else
+        render 'new'
+      end
     end
 
 private
